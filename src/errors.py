8eab9ace19486896ab23c76eb58980b67777d536
Bret <bret.barkley98@gmail.com>
2019-12-21 23:41:11 -0500

    cleanup and refactor

--- src/errors.py
+++ src/errors.py
@@ -1,26 +1,27 @@
 """ Defines things used for error reporting """
 import sys
 
+
 class Error:
-    def __init__(self, fileName):
-        with open(fileName, 'r') as f:
+    def __init__(self, f_name):
+        with open(f_name, 'r') as f:
             self.file = f.read().split('\n')
 
-    def warn(self, msg, errType, lineNum):
-        print( 'Warning:')
-        print( errType + ' error on  line:' + str(lineNum))
-        lineNum -= 1
-        for offset in range(-3, 3):
-            if (lineNum+offset) >= 0 and (lineNum+offset) < len(self.file):
-                pref = '  ' if (offset != 0) else '>>'
-                print( pref + self.file[lineNum + offset])
-        print( msg)
-    def err(self, msg, errType, lineNum): 
-        print( errType + ' error on  line:' + str(lineNum))
-        lineNum -= 1
-        for offset in range(-3, 3):
-            if (lineNum+offset) >= 0 and (lineNum+offset) < len(self.file):
-                pref = '  ' if (offset != 0) else '>>'
-                print( pref + self.file[lineNum + offset])
-        print( msg)
-        sys.exit(0)
+    def message(self, msg_type, msg, e_type, line):
+        print(msg_type)
+        print(e_type + ' error on  line:' + str(line))
+        line -= 1
+        for offset in range(-3 + line, 3 + line):
+            if 0 <= offset < len(self.file):
+                pref = '  ' if (offset != line) else '>>'
+                print(pref + self.file[offset])
+        print(msg)
+
+    def warn(self, cond, msg, e_type, line):
+        if cond:
+            self.message('Warning: ', msg, e_type, line)
+
+    def err_if(self, cond,  msg, e_type, line):
+        if cond:
+            self.message('Error: ', msg, e_type, line)
+            sys.exit(0)
