da77b559e078fd1ffe7486cde6af9a8b36f0ddba
Bret <bret.barkley98@gmail.com>
2019-12-27 15:43:35 -0500

    added logic for conjunctive boolean

--- src/llvmgen.py
+++ src/llvmgen.py
@@ -12,10 +12,12 @@ from llvmlite import ir
 
 i32 = ir.IntType(32)
 fp = ir.DoubleType()
+boolean = ir.IntType(1)
 
 conversions = {
-        'Int': i32,
-        'Float': fp
+    'Int': i32,
+    'Float': fp,
+    'Bool': boolean
 }
 
 
@@ -27,14 +29,33 @@ conversions = {
 includes = ['stdarg', 'stdlib', 'stdio']
 tab = ' '*4
 
-def binop(mod, ast, st, nst, ctx = None):
-    l = codegen(mod, ast[0], st, nst, ctx)
-    r = codegen(mod, ast[2], st, nst, ctx)
-    op = ast[1][0]
+
+def binop(ast, nst, ctx = None):
+    l = codegen(ast[0], nst, ctx)
+    r = codegen(ast[2], nst, ctx)
+    if isinstance(ast[1], AST):
+        op = ast[1][0]
+    else:
+        op = ast[1]
     return (l, r, op)
 
+def conjunctive_bool(ast, nst, ctx):
+    if len(ast) == 1:
+        return codegen(ast[0], nst, ctx)
+    else:
+        l, r, op = binop(ast, nst, ctx)
+        lhs = ctx['builder'].and_(l, r)
+        if op == 'v':
+            f = ctx['builder'].or_
+        elif op == '^':
+            f = ctx['builder'].and_
+        for rhs in asts(ast)[2:]:
+            rhs = codegen(rhs, nst, ctx)
+            lhs = f(lhs, rhs)
+        return lhs
 
-def codegen(mod, ast, st, nst, ctx = None):
+
+def codegen(ast, nst, ctx=None):
     """
     Args:
         ast: AST to read
@@ -46,18 +67,18 @@ def codegen(mod, ast, st, nst, ctx = None):
 
     name = ast.name
     if name == 'program':
-        codegen(mod, ast[0], st, nst, ctx)
+        codegen(ast[0], nst, ctx)
     elif name == 'declaration-list':
         for child in ast:
-            codegen(mod, child, st, nst, ctx)
+            codegen(child, nst, ctx)
     elif name == 'declaration':
-        codegen(mod, ast[0], st, nst, ctx)
+        codegen(ast[0], nst, ctx)
     elif name == 'fun-declaration':
         nst.enterScope(ast[0][0])
         r_type = conversions[ast[-1].type]
         call_sig = tuple(conversions[x] for x in ast[2][0].type)
-        f_type = ir.FunctionType(i32, call_sig)
-        f = ir.Function(mod, f_type, name=ast[0][0])
+        f_type = ir.FunctionType(conversions[ast.returnType], call_sig)
+        f = ir.Function(ctx['mod'], f_type, name=ast[0][0])
         block = f.append_basic_block(name="entry")
 
         builder = ir.IRBuilder(block)
@@ -65,76 +86,66 @@ def codegen(mod, ast, st, nst, ctx = None):
             f.args[i].name = arg_name
         args = f.args
         ctx = {'builder': builder, 'fun': f}
-        codegen(mod, ast[2], st, nst, ctx)
+        codegen(ast[2], nst, ctx)
 
     elif name == 'anonymous-function':
-        codegen(mod, ast[2], st, nst, ctx)
+        codegen(ast[2], nst, ctx)
     elif name == 'final-stmt':
         if ast[0].name == 'other-selection-stmt':
-            codegen(mod, ast[0], st, nst, ctx)
+            codegen(ast[0], nst, ctx)
         else:
-            ctx['builder'].ret(codegen(mod, ast[0], st, nst, ctx))
+            ctx['builder'].ret(codegen(ast[0], nst, ctx))
     elif name == 'expressionList':
         if ast[-1] == 'other-selection-stmt':
             pass
         elif ast[-1] == 'constructor':
             pass
         else:
-            result = codegen(mod, ast[-1], st, nst, ctx)
+            result = codegen(ast[-1], nst, ctx)
         for x in asts(ast[:-1]):
             pass  # DO some kind of lwd/swd
         return result
 
-    elif name == 'boolExprList':
-        if len(ast) == 1:
-            return codegen(mod, ast[0], st, nst, ctx)
-        else:
-            # TODO: or clause
-            pass
-
-    elif name == 'boolTermList':
-        print('boolterm')
-        if len(ast) == 1:
-            return codegen(mod, ast[0], st, nst, ctx)
-        # TODO: multi-and clause
+    elif name in ['boolTermList', 'boolExprList']:
+        return conjunctive_bool(ast, nst, ctx)
 
     elif name == 'boolFactor':
         print('boolfact')
         if ast.case(0):
-            return codegen(mod, ast[0], st, nst, ctx)
+            return codegen(ast[0], nst, ctx)
+        elif ast.case(1):
+            res = codegen(ast[0], nst, ctx)
+            return ctx['builder'].not_(res)
         elif ast.case(2):
-            # TODO: Other cases
-            pass
-        elif ast.case(3):
-            return codegen(mod, ast[1], st, nst, ctx)
+            return codegen(ast[1], nst, ctx)
 
     elif name == 'simple-expression':
         if ast.case(0):
-            l, r, op = binop(mod, ast, st, nst, ctx)
+            l, r, op = binop(ast, nst, ctx)
             return ctx['builder'].icmp_signed(op, l, r)
         else:
-            return codegen(mod, ast[0], st, nst, ctx)
+            return codegen(ast[0], nst, ctx)
     elif name == 'additive-expression':
         if ast.case(0):
-            l, r, op = binop(mod, ast, st, nst, ctx)
+            l, r, op = binop(ast, nst, ctx)
             if op == '+':
                 return ctx['builder'].add(l, r)
             elif op == '-':
                 return ctx['builder'].sub(l, r)
         else:
-            return codegen(mod, ast[0], st, nst, ctx)
+            return codegen(ast[0], nst, ctx)
     elif name == 'term':
         if ast.case(0):
-            l, r, op = binop(mod, ast, st, nst, ctx)
+            l, r, op = binop(ast, nst, ctx)
             if op == '*':
                 return ctx['builder'].mul(l, r)
             elif op == '/':
                 return ctx['builder'].sdiv(l, r)
         else:
-            return codegen(mod, ast[0], st, nst, ctx)
+            return codegen(ast[0], nst, ctx)
     elif name == 'factor':
         if ast.case(0):
-            return codegen(mod, ast[1], st, nst, ctx)
+            return codegen(ast[1], nst, ctx)
         elif ast.case(1):  # Var access:
             # Param case
             p = [x for x in ctx['fun'].args if x.name == ast[0][0]][0]
@@ -175,7 +186,7 @@ valid = errorCheck(flattenedListAST,
 
 
 module = ir.Module(name='Default')
-codegen(module, flattenedListAST, st, nst)
+codegen(flattenedListAST, nst, {'mod': module})
 print(module)
 print(module.functions)
 
